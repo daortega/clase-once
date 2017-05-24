ba335c018d6d1c46c9d32eaf64f808d5e7e75840
Denisse Ortega <daortega@uc.cl>
2017-05-24 13:59:51 -0400

    subir todo
    Por alguna razón misteriosa, los títulos no se leen.

---
+++ about.php
@@ -0,0 +1,52 @@
+<?php include('header.php')?>
+<div class="row">
+<div class="col-sm-12">
+<h3>Sobre Portafolio</h3>
+<p> Por ahora, quiero hacer un portafolio decente para postular a práctica de oficina. Lo ideal sería que no se vea muy sobrecargada de cosas y que quien la vea pueda
+hallar el contenido que le interese y sea pertinente para los objetivos de la práctica</p>
+<p> Sería ideal que se pueda discriminar entre proyectos "gráficos, industriales, etc", ya que se suelen buscar practicantes con alguno de esos tipos de perfil y personalmente no me podría catalogar en ninguna. </p>
+<p> El orden en el que vean mis proyectos afectará en la impresión que tengan los visitantes, y hasta ahora me han dicho que soy industrial.</p>
+<p> Entre mis antecedentes de portafolios puse varias opciones:
+<ul>
+<li> La ideal, y que veo difícil de hacer, que sería el de Nahel Moussi. </li>
+<li> Una opción intermedia que podría ser la de Pulse.</li>
+<li> La que se ve más factible de realizar que es la de Bootstrap. </li>
+</ul>
+  </p>
+<p> A continuación van unas pocas aclaraciones de cosas que quiero rescatar de mi lista de antecedentes y referentes:</p>
+<br>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6">
+  <figure>
+    <img src="images/ejemplo-06.jpg">
+    <figcaption> Referente: <a href="https://cl.pinterest.com/pin/317855686187199880/" target="_blank">Pinterest</a></figcaption>
+    <figure>
+</div>
+<div class="col-sm-6">
+  <h3>Cosas Gráficas</h3>
+  <p> Por el momento quisiera usar colores así para mi sitio, pero no estoy muy segura así que quizás la cambie después.</p>
+  <p> Sobre tipografía, algo que sea simple sería ideal, incluyendo en los títulos. </p> 
+  <br>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-sm-6">
+  <figure>
+    <img src="images/ejemplo-03.jpg">
+    <figcaption> Referente: <a href="http://nahelmoussi.com/" target="_blank">Nahel Moussi Portfolio</a></figcaption>
+    <figure>
+</div>
+<div class="col-sm-6">
+  <h3>Index</h3>
+  <p> Quisiera que la primera visualización que se tenga del portafolio sea una vista simple de mis proyectos. Algo así como en esta página a la izquierda. </p>
+  <br>
+  <h4>Dificultades que veo</h4>
+  <p> Tiene una forma de desplazarse por los distintos trabajos que no sé cómo se hace. Aparece un círculo que puedes arrastrar por la pantalla, algo así como si fuese el dedo en la pantalla del celular. </p>
+  <p> Por otro lado, tiene varios efectos como cuando seleccionas un proyecto. En verdad no será literalmente así, pero quizás el hecho de que no tuviese esos efectos va a modificar mucho esa fluidez que percibo del sitio. </p>
+</div>
+</div>
+
+<?php include('footer.php')?>
